a9de758c11a1f6a99ed8efd3587febdfc199f644
Rakesh Prashant Mal <malrakesh172@gmail.com>
2024-03-18 23:04:29 +0530

    loop commited

---
+++ loop.java
@@ -0,0 +1,10 @@
+public class loop {
+    public static void main(String[] args) {
+        System.out.println("*** JAVA While Loop ***");
+        int i = 0;
+        while(i<5){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
