03b733de278b78dcb95bf5cb72dcb1e0871984d2
Rakesh Prashant Mal <malrakesh172@gmail.com>
2024-03-18 23:10:34 +0530

    loop changed

--- loop.java
+++ loop.java
@@ -2,9 +2,14 @@ public class loop {
     public static void main(String[] args) {
         System.out.println("*** JAVA While Loop ***");
         int i = 0;
-        while(i<5){
+        //  the loop will run, over and over again, as long as a variable (i) is less than 10
+        while(i<10){
             System.out.println(i);
             i++;
         }
     }
 }
+
+
+
+
