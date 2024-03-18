0fcaf4e3b33c53577422bdb29c77f1eb1dbae7a5
Rakesh Prashant Mal <malrakesh172@gmail.com>
2024-03-18 23:13:13 +0530

    comment commited

--- loop.java
+++ loop.java
@@ -2,6 +2,7 @@ public class loop {
     public static void main(String[] args) {
         System.out.println("*** JAVA While Loop ***");
         int i = 0;
+        // while loop runs through a block of code as long as a specified condition is true
         //  the loop will run, over and over again, as long as a variable (i) is less than 10
         while(i<10){
             System.out.println(i);
