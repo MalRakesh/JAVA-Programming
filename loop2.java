87f76abc36d8ae5ae89aaa013e7eef933a60f291
Rakesh Prashant Mal <malrakesh172@gmail.com>
2024-03-20 01:24:03 +0530

    initial commit

---
+++ loop2.java
@@ -0,0 +1,12 @@
+public class loop2 {
+    public static void main(String[] args) {
+        System.out.println("*** JAVA For Loop ***");
+        // When we know exactly how many times we want to loop, we use for loop
+        for(int i = 0; i < 8; i++){
+            System.out.println(i);
+        }
+    }
+}
+
+
+
---
+++ loop3.java
@@ -0,0 +1,16 @@
+public class loop3 {
+    public static void main(String[] args) {
+        System.out.println("*** Java Nested Loop ***");
+        // The "inner loop" will be executed one time for each iteration of the "outer loop"
+        for(int i=1;i<=2;i++){
+            System.out.println("Parent: " + (i));
+
+        for(int j=1;j<=3;j++){
+            System.out.println("Child: "+(j));
+        }
+        }
+    }
+}
+
+
+
