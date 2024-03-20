3aa9a4a56ac098daf98648d2152fbee8b101fa46
Rakesh Prashant Mal <malrakesh172@gmail.com>
2024-03-20 22:55:35 +0530

    initial commit

---
+++ BreakConcept.java
@@ -0,0 +1,15 @@
+public class BreakConcept {
+    public static void main(String[] args) {
+        System.out.println("*** JAVA Break Concept ***");
+        // break statement is used to jump out of a loop
+        for (int i = 0; i < 10; i++) {
+            if (i == 6) {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
+
+
+
---
+++ loop4.java
@@ -0,0 +1,13 @@
+public class loop4 {
+    public static void main(String[] args) {
+        System.out.println("*** JAVA For Each Loop ***");
+        // For Each Loop is used exclusively to loop through elements in an array
+        String[] bike = {"Honda","Spendor","Pulser","Yamaha"};
+        for(String i : bike){
+            System.out.println(i);
+        }
+    }
+}
+
+
+
