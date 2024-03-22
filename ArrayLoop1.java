e1bb4a09b6f1a1a759685e28a667fc54223b307f
Rakesh Prashant Mal <malrakesh172@gmail.com>
2024-03-23 01:26:17 +0530

    initial commit

--- ArrayLoop1.java
+++ ArrayLoop1.java
@@ -1,6 +1,6 @@
 public class ArrayLoop1 {
     public static void main(String[] args) {
-        System.out.println("*** JAVA Arrays Loop ***");
+        System.out.println("*** JAVA Array Loop Concept ***");
         // Java Array using For Each Loop
         String[] Lang = {"JAVA","C","C++","Python"};
         for (String i : Lang ){
