7feb6784cce3a2dbe303ceb0d633d1708ed96b06
Rakesh Prashant Mal <malrakesh172@gmail.com>
2024-03-23 01:22:08 +0530

    initial command

---
+++ ArrayLoop.java
@@ -0,0 +1,10 @@
+public class ArrayLoop {
+    public static void main(String[] args) {
+        System.out.println("*** JAVA Array Loop Concept ***");
+        // Java For loop used, and the length property to specify how many times the loop should run
+        String[] Bike = {"Honda","Yamaha","TVS","Kawasaki","Pulser"};
+        for (int i = 0; i < Bike.length ;i++){
+            System.out.println(Bike[i]);
+        }
+    }
+}
---
+++ ArrayLoop1.java
@@ -0,0 +1,10 @@
+public class ArrayLoop1 {
+    public static void main(String[] args) {
+        System.out.println("*** JAVA Arrays Loop ***");
+        // Java Array using For Each Loop
+        String[] Lang = {"JAVA","C","C++","Python"};
+        for (String i : Lang ){
+            System.out.println(i);
+        }
+    }
+}
