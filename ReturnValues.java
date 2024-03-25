c00e2b6203e0cfb603b53356257858452a9c4a54
Rakesh Prashant Mal <malrakesh172@gmail.com>
2024-03-25 23:20:28 +0530

    initial commit

---
+++ ReturnValues.java
@@ -0,0 +1,13 @@
+public class ReturnValues {
+    // instead of void keyword I have used here int keyword because, I want to return a value
+    static int myMethod(int x) {
+        return 6 + x;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(myMethod(8));
+    }
+
+}
+
+
