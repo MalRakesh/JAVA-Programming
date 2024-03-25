667883538a802c102924cd69fbba525048644138
Rakesh Prashant Mal <malrakesh172@gmail.com>
2024-03-25 19:57:49 +0530

    initial commit

---
+++ Parameters.java
@@ -0,0 +1,17 @@
+public class Parameters {
+    static void myMethod(String Course) {
+        System.out.println(Course + " is a Coding Skills.");
+    }
+    public static void main(String[] args) {
+        System.out.println("*** JAVA Parameters Concept ***");
+        myMethod("JAVA");
+        myMethod("Python");
+        myMethod("C Programming");
+        myMethod("C ++");
+    }
+}
+
+
+
+
+
