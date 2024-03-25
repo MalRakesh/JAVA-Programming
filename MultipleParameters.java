8d2f547325d8eb8acc539d07851c339c05a92c7d
Rakesh Prashant Mal <malrakesh172@gmail.com>
2024-03-25 23:05:50 +0530

    initial commit

--- MultipleParameters.java
+++ MultipleParameters.java
@@ -1,3 +1,14 @@
 public class MultipleParameters {
-    
+    static void myMethod(String Name,int age){
+        System.out.println(Name + " is " + age + " yrs old.");
+    }
+    public static void main(String[] args) {
+        System.out.println("*** Multiple Parameters JAVA Concept ***");
+        myMethod("Rakesh", 20);
+        myMethod("Rahul", 24);
+        myMethod("Rajiv", 29);
+    }
 }
+
+
+
