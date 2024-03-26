a5c125966a6d16270f326819a66c32f319ddeb40
Rakesh Prashant Mal <malrakesh172@gmail.com>
2024-03-26 21:45:52 +0530

    initial commit

---
+++ MethodOverloading.java
@@ -0,0 +1,19 @@
+public class MethodOverloading {
+    static int myMethodInt(int x,int y){
+        return x + y;
+    }
+    static double myMethodDouble(double x,double y){
+        return x + y;
+    }
+    public static void main(String[] args) {
+        System.out.println("*** JAVA Method Overloading Concept ***");
+        int num1 = myMethodInt(14,61);
+        double num2 = myMethodDouble(4.3, 6.2 );
+        System.out.println("Int : " + num1);
+        System.out.println("Double : " + num2);
+    }
+}
+
+
+
+
