416ed1f7ea9ffd4202e561fe42c443544f04b9e6
Rakesh Prashant Mal <malrakesh172@gmail.com>
2024-03-26 21:58:17 +0530

    initial commit

--- Scope.java
+++ Scope.java
@@ -2,7 +2,7 @@ public class Scope {
     public static void main(String[] args) {
         // A block of code refers to all of the code between curly braces {}
         {   // block code starts here
-            System.out.println("*** JAVA Method Scope Concept ***");
+            System.out.println("*** JAVA Block Scope Concept ***");
             int a = 21;
             System.out.println("The Value of a : " + (a));
         }   // block code ends here
