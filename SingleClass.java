a483cd9562b68ea3acecee6831e2924f38ed9736
Rakesh Prashant Mal <malrakesh172@gmail.com>
2024-03-28 22:19:06 +0530

    initial commit

---
+++ SingleClass.java
@@ -0,0 +1,12 @@
+public class SingleClass {
+    int a = 16;
+    public static void main(String[] args) {
+        System.out.println("*** JAVA Single Class ***");
+        SingleClass obj1 = new SingleClass();
+        System.out.println(obj1.a);
+    }
+    
+}
+
+
+
