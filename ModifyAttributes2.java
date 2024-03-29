74ffa6f1c15d841003480541aea1cdce40b97eee
Rakesh Prashant Mal <malrakesh172@gmail.com>
2024-03-29 23:10:50 +0530

    initial commit

--- ModifyAttributes2.java
+++ ModifyAttributes2.java
@@ -1,10 +1,10 @@
-public class ModifyAttributes3 {
+public class ModifyAttributes2 {
     // If we don't want the ability to override existing values, declare the attribute as final
     final int y = 2;
 
     public static void main(String[] args) {
         System.out.println("*** JAVA Modify Attributes Concept ***");
-        ModifyAttributes3 obj3 = new ModifyAttributes3();
+        ModifyAttributes2 obj3 = new ModifyAttributes2();
         // obj3.y = 99;     // occur an error: cannot assign a value to a final variable
         System.out.println(obj3.y);
     }
