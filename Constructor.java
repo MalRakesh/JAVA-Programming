67c399d7b6856ef898357581b87b75ec19feed5e
Rakesh Prashant Mal <malrakesh172@gmail.com>
2024-03-31 21:34:37 +0530

    initial commit

--- Constructor.java
+++ Constructor.java
@@ -5,11 +5,10 @@ public class Constructor {
     public Constructor(){
         a = 23;
     }
-    
+
     public static void main(String[] args) {
         System.out.println("*** JAVA Constructor Concept ***");
-    //Create an object of class Main (This will call the constructor)
-        Constructor obj1 = new Constructor();  
+        Constructor obj1 = new Constructor();  //Create an object of class Main (This will call the constructor)
         System.out.println(obj1.a);
     }
 }
